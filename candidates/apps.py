0b55f12bcc2fd903976945a0298f89847410b9d8
Ruhaan Hawaldar <ruhaanhawaldar@gmail.com>
2026-07-07 18:24:29 +0530

    Auto create HR admin

--- candidates/apps.py
+++ candidates/apps.py
@@ -1,14 +1,24 @@
+from django.apps import AppConfig
 import os
-from django.contrib.auth.models import User
 
-username = os.getenv("ADMIN_USERNAME")
-email = os.getenv("ADMIN_EMAIL")
-password = os.getenv("ADMIN_PASSWORD")
 
-if username and password:
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(
-            username=username,
-            email=email,
-            password=password,
-        )
+class CandidatesConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "candidates"
+
+    def ready(self):
+        from django.apps import apps
+
+        User = apps.get_model("auth", "User")
+
+        username = os.getenv("ADMIN_USERNAME")
+        email = os.getenv("ADMIN_EMAIL")
+        password = os.getenv("ADMIN_PASSWORD")
+
+        if username and password:
+            if not User.objects.filter(username=username).exists():
+                User.objects.create_superuser(
+                    username=username,
+                    email=email,
+                    password=password,
+                )
